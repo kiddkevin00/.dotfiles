4812bd97f526ec0712659a080bb48f7347b4a058
Marcus Hsu <mh3346@columbia.edu>
2022-11-08 18:39:49 -0800

    Fix bootstrap script

--- android/path.zsh
+++ android/path.zsh
@@ -1,7 +1,7 @@
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+# export ANDROID_HOME=$HOME/Library/Android/sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/tools
+# export PATH=$PATH:$ANDROID_HOME/tools/bin
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-export MANPATH=$MANPATH:$(/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home)/man
+# export MANPATH=$MANPATH:$(/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home)/man
