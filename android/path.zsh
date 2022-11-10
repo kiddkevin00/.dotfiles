8e22eb89198ad3ad1d8d27f5990bde570da32930
Marcus Hsu <mh3346@columbia.edu>
2022-11-10 11:19:55 -0800

    Update Android env setup

--- android/path.zsh
+++ android/path.zsh
@@ -1,7 +1,5 @@
-# export ANDROID_HOME=$HOME/Library/Android/sdk
-# export PATH=$PATH:$ANDROID_HOME/emulator
-# export PATH=$PATH:$ANDROID_HOME/tools
-# export PATH=$PATH:$ANDROID_HOME/tools/bin
-# export PATH=$PATH:$ANDROID_HOME/platform-tools
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
 
 # export MANPATH=$MANPATH:$(/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home)/man
