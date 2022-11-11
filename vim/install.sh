f0cb24d64cfc5bff699eabcb75d26969014e663b
Marcus Hsu <mh3346@columbia.edu>
2022-11-10 19:05:12 -0800

    Override the default Vim with the latest one and always install its plugins

--- vim/install.sh
+++ vim/install.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env sh
 #
 # Vim
-# This installs vim and vim plugins (using vundle)
+# This installs vim and vim plugins (using Vundle)
 
+echo " Override the default Vim for you."
 
-# check for vim
-if ! type "vim" &> /dev/null;
-then
-  echo " Installing Vim for you."
+# install vim via brew (the default one is usually broken)
+brew install vim
 
-  # install vim
-  brew install vim
-
-  # install vundle/vundlePlugins
-  $DOTZ/vim/vundle-install.sh 2>&1
-fi
+# install vundle/vundlePlugins
+$DOTZ/vim/vundle-install.sh 2>&1
 
 # create temp files used by vim
 mkdir -p $HOME/.vim/tmp/{backup,swap,tags,undo}
