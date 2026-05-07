276a908098d0cfe344066cf89c371b30c392208b
Marcus Hsu <mh3346@columbia.edu>
2026-05-07 11:03:43 -0600

    fix: use Homebrew prefixes for shell tool paths
    * Update zsh config to resolve Homebrew, asdf,
      MySQL client, and manpath locations from the
      active Homebrew prefix instead of hardcoded
      Intel or Apple Silicon paths.

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,12 @@
-export MANPATH="/usr/share/man:/usr/local/man:/usr/local/share/man:$MANPATH"
+typeset -U manpath
+
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  manpath=(/usr/share/man "$HOMEBREW_PREFIX/man" "$HOMEBREW_PREFIX/share/man" $manpath)
+  unset HOMEBREW_PREFIX
+else
+  manpath=(/usr/share/man $manpath)
+fi
+
+export MANPATH
